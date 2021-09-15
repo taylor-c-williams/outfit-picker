4303de3c42bc25f6c7814318d443ad984655c39c
Karl Sigel <heykarl@gmail.com>
2021-09-15 15:18:39 -0700

    added event listener to first page button that checks number of selected shirts and pants and displays error if less than 3, otherwise takes to closet.

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 import { populateTops, populatePants } from './populate-clothes.js';
-import { getSelectedClothing } from './utils.js';
+import { checkSelectedTops, checkSelectedPants } from './check-number-selected.js';
 
 populateTops();
 populatePants();
+
+const toDressingRoomBtn = document.getElementById('choices-submit');
+
+toDressingRoomBtn.addEventListener('click', () => {
+
+    const numberOfTops = checkSelectedTops();
+    const numberOfPants = checkSelectedPants();
+
+    if (numberOfTops < 3) {
+        alert('more tops!');
+    } else if (numberOfPants < 3) {
+        alert('more pants!');
+    } else window.location = './results';
+}
+);
--- check-number-selected.js
+++ check-number-selected.js
@@ -1,8 +1,17 @@
 import { getSelectedClothing } from './utils.js';
 
-const selectedClothing = getSelectedClothing();
-const getSelectedShirts = selectedClothing.filter((item) => {
-    if (item.category === 'top') {
-        return item.length;
-    }
-});
+export function checkSelectedTops() {
+    const selectedClothing = getSelectedClothing();
+    const getSelectedShirts = selectedClothing.filter((item) => 
+        (item.category === 'top') 
+    );
+    return getSelectedShirts.length;
+}
+
+export function checkSelectedPants() {
+    const selectedClothing = getSelectedClothing();
+    const getSelectedPants = selectedClothing.filter((item) => 
+        (item.category === 'pants') 
+    );
+    return getSelectedPants.length;
+}
