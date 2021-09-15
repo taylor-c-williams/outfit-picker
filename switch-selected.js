db96a5d9ccfd8d5d624d29f0b878b1880a9fcd44
taylor-c-williams <taylor.callan.williams@gmail.com>
2021-09-15 16:33:54 -0700

    disabled dressing room nav on homepage

--- switch-selected.js
+++ switch-selected.js
@@ -4,10 +4,6 @@ export function switchSelected(id) {
     const clothing = getClothing();
     const singleClothing = clothing.find((item) => item.id === id);
     singleClothing.selected = !singleClothing.selected;
-    // if (singleClothing.selected === true) {
-    //     singleClothing.selected = false;
-    // } else {
-    //     singleClothing.selected = true;
-    // }
+    
     setClothing(clothing);
 }
