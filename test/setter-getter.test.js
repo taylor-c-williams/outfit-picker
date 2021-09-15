a9cff00d2eab06418e76cd01544fd1e536a7f387
taylor-c-williams <taylor.callan.williams@gmail.com>
2021-09-15 14:53:01 -0700

    getClothing passing test

--- test/setter-getter.test.js
+++ test/setter-getter.test.js
@@ -1,7 +1,9 @@
-import { setClothing } from '../utils.js';
+import { setClothing, getClothing } from '../utils.js';
 
 const test = QUnit.test;
 
+
+// setClothing
 test('setClothing should take an array of clothing, string it and store it in local ', (expect) => {
 
     const taylorsCloset = [
@@ -29,4 +31,33 @@ test('setClothing should take an array of clothing, string it and store it in lo
     const actual = unstrungClothes;
 
     expect.deepEqual(actual, expected);
+});
+
+
+// getClothing
+test('getClothing should return an array of clothing items which has been stored in local', (expect) => {
+    
+    const closet = [
+        {
+            name:'top-3',
+            id: 7,
+            image: 'tops03.jpg',
+            selected: false,
+            category: 'top',
+        },
+        {
+            name:'top-4',
+            id: 8,
+            image: 'tops04.jpg',
+            selected: false,
+            category: 'top',
+        },
+    ];    
+
+    const stringyCloset = JSON.stringify(closet);
+    localStorage.setItem('CLOSET', stringyCloset);
+
+    const actual = getClothing();
+
+    expect.deepEqual(actual, closet);
 });
